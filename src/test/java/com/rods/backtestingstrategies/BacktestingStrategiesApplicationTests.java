b4ca657e02189ce4d3f92349cb2c896e9595bb98
Sian Rodrigues <sianrods250@gmail.com>
2026-01-15 09:26:21 +0530

    jar file added for deployment

--- src/test/java/com/rods/backtestingstrategies/BacktestingStrategiesApplicationTests.java
+++ src/test/java/com/rods/backtestingstrategies/BacktestingStrategiesApplicationTests.java
@@ -3,7 +3,13 @@ package com.rods.backtestingstrategies;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+        properties = {
+                "spring.autoconfigure.exclude=" +
+                        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+                        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+        }
+)
 class BacktestingStrategiesApplicationTests {
 
     @Test
